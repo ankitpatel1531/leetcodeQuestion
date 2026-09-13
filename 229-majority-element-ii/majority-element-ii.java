b35509d9b3af1831e87cdefdc067ba10a05ac36e
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-13 22:08:56 +0530

    Time: 14 ms (43.87%) | Memory: 53 MB (19.36%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -17,31 +17,3 @@ class Solution {
         
     }
 }
-// import java.util.*;
-
-// class Solution {
-//     public List<Integer> majorityElement(int[] nums) {
-
-//         int n = nums.length;
-
-//         HashMap<Integer, Integer> map = new HashMap<>();
-
-//         // Count frequency
-//         for (int i = 0; i < n; i++) {
-
-//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-//         }
-
-//         // Find elements appearing more than n/3 times
-//         List<Integer> result = new ArrayList<>();
-
-//         for (int key : map.keySet()) {
-
-//             if (map.get(key) > n / 3) {
-//                 result.add(key);
-//             }
-//         }
-
-//         return result;
-//     }
-// }
